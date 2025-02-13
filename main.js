cc54d61ac0332735b099f1635287011a3c65664e
Carl Nathan <darkbuilderop@gmail.com>
2025-02-13 18:42:26 +0800

    Update main.js
    Signed-off-by: Carl Nathan <darkbuilderop@gmail.com>

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('thank you sa lahat maa -ortizpogi123').split('')
+    const titles = ('thank you sa lahat - ortizpogi123').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
